2232e3932178f03fc6394696ceb0c29c1a232e03
mihaela1354 <mihaeladinkova@abv.bg>
2020-06-26 22:42:01 +0300

    fix: header

--- bulgarian-mountains/src/Header/Header.js
+++ bulgarian-mountains/src/Header/Header.js
@@ -2,15 +2,13 @@ import React, { Component } from 'react';
 import Auth from './Auth';
 import Navbar from './Navbar';
 
-class Header extends Component {
-    render() {
-        return (
-            <header>
-                <Auth />
-                <Navbar />
-            </header>
-        )
-    }
+const header = () => {
+    return (
+        <header>
+            <Auth />
+            <Navbar />
+        </header>
+    )
 }
 
-export default Header;
+export default header;
