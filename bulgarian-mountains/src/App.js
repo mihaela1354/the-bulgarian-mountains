6969c31dcb8b5d6af826576064a3b89a9be1b5e8
mihaela1354 <mihaeladinkova@abv.bg>
2020-07-01 20:02:03 +0300

    feat: hut

--- bulgarian-mountains/src/App.js
+++ bulgarian-mountains/src/App.js
@@ -2,10 +2,16 @@ import React, { Component } from 'react';
 import './styles/App.css';
 import './styles/typography.css'
 import Header from './Header/Header';
+import Hut from './Hut/Hut';
 
 class App extends Component {
   state = {
-    text: "Some text"
+    text: "Some text",
+    huts: [{
+      name: "Seven Rila Lakes",
+      grade: 5.6,
+      description: "Pariatur nostrud occaecat cupidatat non excepteur esse eiusmod adipisicing. Exercitation in cupidatat adipisicing nisi cupidatat quis sunt. Excepteur id magna labore laborum qui veniam Lorem occaecat nulla labore."
+    }]
   }
   changeHandler = (event) => {
     this.setState({
@@ -17,6 +23,9 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
+        <main>
+          <Hut hut={this.state.huts[0]}/>
+        </main>
       </div>
     );
   }
