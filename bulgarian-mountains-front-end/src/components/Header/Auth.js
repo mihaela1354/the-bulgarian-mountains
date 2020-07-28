ca6513f293f62f98ea1845bc33ddbafc741327be
mihaela1354 <mihaeladinkova@abv.bg>
2020-07-29 00:04:58 +0300

    add home page link

--- bulgarian-mountains-front-end/src/components/Header/Auth.js
+++ bulgarian-mountains-front-end/src/components/Header/Auth.js
@@ -5,7 +5,7 @@ import styles from './auth.module.css';
 const auth = (props) => {
     return (
         <div style={styles} className="auth">
-            <h1><a>BulHuts</a></h1>
+            <h1><Link to="/">BulHuts</Link></h1>
             {!props.logged ?
                 <div>
                     <Link to="/login">Login</Link>
